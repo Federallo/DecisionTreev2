2bf431204b8899dd9030dc377e64d488ead3415e
Federallo <aligiassioma72@gmail.com>
2024-01-26 17:03:24 +0000

    added first implementation of information gain function

--- decision_tree.py
+++ decision_tree.py
@@ -2,6 +2,9 @@
 from ucimlrepo import fetch_ucirepo
 import pandas as pd
 
+# other libraries
+import math
+
 # defining tree nodes data structure
 class Node:
     def __init__(self, name, branch, type_name):
@@ -16,23 +19,23 @@ class DecisionTree:
 
     # initialising decision tree
     def __init__(self, examples):
-        # creating the list of attributes
+        self.depth = 0
         self.root = self.decision_tree_learning(examples, attributes, {}) # parents_examples is empty because we are starting from an empty tree
 
     # defining recursive decision tree learning algorithm
     def decision_tree_learning(examples, attributes, parent_examples):
         # checks if there are no examples left
         if examples.empty:
-            return self.plurality_value(parent_examples)
+            return self.plurality_value(parent_examples.targets)
         # checks if all examples have the same outcome
-        if len(examples.targets.value_counts()) == 1:
+        else if examples.targets.value_counts() == 1:
             return examples.targets.value_counts() # TODO must be changed to the effective outcome name
         # checks if there are no attributes left in the examples
-        if len(examples.features.value_counts()) is empty:
-            return self.plurality_value(examples)
+        if examples.features.value_counts() is empty:
+            return self.plurality_value(examples.targets)
         # defining the most important attribute and adding it to the tree as a node
         else:
-            A =# TODO create a function that chooses the most important attribute among the others
+            A = max_importance(examples)# here we choose the most important attribute among the others
             tree = Node(#A.name, A.branch, node)
             for values in A:
                 # exs = e must belong to examples and e.A=v # TODO find the meaning of this line
@@ -40,7 +43,6 @@ class DecisionTree:
                 # TODO add a branch to tree with label (A=v) and as subtree 'subtree'
     return tree
                 
-
     # defining the function that selects the most common output value
     # FIXME must count only the most common outcome among the examples
     def plurality_value(examples):
@@ -50,6 +52,26 @@ class DecisionTree:
                 most_common = unique_value
         return most_common
 
+    # defining the most important attribute among the other by using information gain
+    def max_importance(examples):
+        entropy_set = entropy(examples.targets) # computing the entropy of the entire set
+        current_attribute = examples.features[0]
+        information_gain = entropy_set - entropy(current_attribute) # information gain of the first attribute
+        # determing the attribute with highest information gain
+        for attribute in examples.features:
+            current = entropy_set - entropy(attribute) # computing the entropy of single attributes
+            if current > information_gain:
+                information_gain = current
+                current_attribute = attribute
+        return current_attribute
+    
+    # defining the entropy of a set
+    def entropy(set):
+        total = 0
+        for i = 0 to len(set.value_counts())-1 #the indices of values
+            total -= len(set[i])/len(set)*math.log2(len(set[i])/len(set)) # where the fraction represent the distribution of datasets' values
+                                                                          # (which are targets' or attributes')
+        return total
 
 # TODO add this part into another file. Tree class definitions must be in a separate file and every test for dataset must be done in a different file
 iris = fetch_ucirepo(id=53)
