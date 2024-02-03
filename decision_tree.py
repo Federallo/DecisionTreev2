9dde95a96087c2431a6bdbc0d0ba46a2adf387b2
Federallo <aligiassioma72@gmail.com>
2024-02-03 17:13:07 +0100

    moved out branch and node classes

--- decision_tree.py
+++ decision_tree.py
@@ -7,17 +7,8 @@ import math
 import networkx
 import matplotlib.pyplot as plt
 
-# defining branch data class in order to contain the branch's label and it subtree
-class Branch:
-    def __init__(self):
-        self.label = []
-        self.subtree = []
-
-# defining tree nodes data structure
-class Node:
-    def __init__(self, name):
-        self.attribute_name = name # attribute name for the node
-        self.branch = Branch() # list of node's branches (attribute's possible values)
+# import custom libraries
+from tree_elements import Branch, Node
 
 # defining the decision tree data structure 
 class DecisionTree:
---
+++ tree_elements.py
@@ -0,0 +1,11 @@
+# defining branch data class in order to contain the branch's label and it subtree
+class Branch:
+    def __init__(self):
+        self.label = []
+        self.subtree = []
+
+# defining tree nodes data structure
+class Node:
+    def __init__(self, name):
+        self.attribute_name = name # attribute name for the node
+        self.branch = Branch() # list of node's branches (attribute's possible values)
