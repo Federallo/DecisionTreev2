c35739ad5619289cef9dc2417cfaba0b294d723e
Federallo <aligiassioma72@gmail.com>
2024-02-03 13:14:23 +0100

    fixed tree testing in case it has depth of length 1

--- decision_tree.py
+++ decision_tree.py
@@ -147,18 +147,18 @@ def test_tree(decision_tree, test_data, target):
 
 # calculating tree precision on a single row
 def tree_precision(tree, row, target):
-    """
+    
     # checking if the root attribute value in case we chose a tree of depth 1 (i.e. the tree is only a leaf)
     if tree == list(row[list(row)[-1]])[0] and tree in target:
         return True
-    else:
+    elif tree in target:
         return False
-    """
+
     # confronting the tree outcome with the training/test example
     for i in range(0, len(tree.branch.label)):
         if list(row[tree.attribute_name])[0] == tree.branch.label[i]:
             # checking if the branch is connected to a leaf
-            if tree.branch.subtree[i] in target: # TODO target must be a list
+            if tree.branch.subtree[i] in target:
                 # checking if test data target is equal to decision tree one
                 if list(row[list(row)[-1]])[0] == tree.branch.subtree[i]:
                     return True
