c07c4290cb15ea4ca9d18666cbd2f42d399f5af3
Federallo <aligiassioma72@gmail.com>
2024-02-06 15:34:41 +0100

    updated precisions prints and added some comments

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from tree_creation import create_tree
 def main():
     dataset_id = 53
     test_amount_examples = 30
-    tree_max_depth = 100
-    examples_lower_bound = 0
+    tree_max_depth = 100 # P
+    examples_lower_bound = 0 # M
     create_tree(dataset_id, test_amount_examples, tree_max_depth, examples_lower_bound)
 
 if __name__ == '__main__':
