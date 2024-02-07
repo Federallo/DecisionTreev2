42f4f619abf05681bdfaaac20bab466287bfb3b2
Federallo <aligiassioma72@gmail.com>
2024-02-07 20:34:24 +0100

    added datasets' ids and descriptions as comments

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from tree_creation import create_tree
 
 def main():
-    dataset_id = 53
+    # dataset: 53 for iris, 109 for wine, 519 for heart failure clinical records 
+    # sizes: 150 for iris, 178 for wine, 299 for heart failure clinical records
+    dataset_id = 109
     test_amount_examples = 30
     tree_max_depth = 100 # P
     examples_lower_bound = 0 # M
