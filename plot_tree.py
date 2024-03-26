9d2862b3233fe2cef3d0c89ad32120d46df3e9cc
Federallo <aligiassioma72@gmail.com>
2024-03-26 18:28:42 +0100

    fixed tree plot at depth 1

--- plot_tree.py
+++ plot_tree.py
@@ -1,7 +1,17 @@
 import networkx
 
+def plot_tree(decision_tree, target, counter, multigraph):
+    # checking if the tree has a leaf as root
+    if not hasattr(decision_tree, 'branch'):
+        root_id = f"{counter}_root"
+        multigraph.add_node(root_id, label = decision_tree) # because it is a leaf
+        counter += 1
+        return root_id, counter
+    else:
+        plot_tree_recursive(decision_tree, target, counter, multigraph)
+
 # defining plotting tree function
-def plot_tree(decision_tree, target, counter, multigraph): # where target is the list of the possible dataset's targets
+def plot_tree_recursive(decision_tree, target, counter, multigraph): # where target is the list of the possible dataset's targets
     # adding unique identifiers in order to have separate nodes and branches
     root_id = f"{counter}_root"
     multigraph.add_node(root_id, label = decision_tree.attribute_name)
@@ -15,7 +25,7 @@ def plot_tree(decision_tree, target, counter, multigraph): # where target is the
             counter += 1
         # recursive call for the subtrees
         else:
-            node_id, counter = plot_tree(decision_tree.branch.subtree[i], target, counter, multigraph) # keeping track of counter in order to have separate nodes
+            node_id, counter = plot_tree_recursive(decision_tree.branch.subtree[i], target, counter, multigraph) # keeping track of counter in order to have separate nodes
             multigraph.add_edge(root_id, node_id, label = decision_tree.branch.label[i])
 
     return root_id, counter
